80d756bf7cebe1585bcf23ee253130472934dcc0
Suede <ymp17kwu@uea.ac.uk>
2020-01-29 23:59:29 +0000

    Fixed issues after merge
    ~ Fixed issue with duplicate code

--- src/misc.rs
+++ src/misc.rs
@@ -1,3 +1,4 @@
+#[derive(Clone)]
 pub struct Length {
     length: u32
 }
@@ -46,29 +47,4 @@ mod tests {
 
         assert_eq!(length.get_length(), 120);
     }
-}
-
-#[derive(Clone)]
-pub struct Length {
-    length: u32
-}
-
-impl Length {
-    pub fn from_int(x: u32) -> Length {
-        let length: Length = Length {
-            length: x
-        };
-
-        return length;
-    }
-
-    pub fn pretty_print(&self) -> String {
-        let hours = self.length / 3600;
-        let minutes = (self.length % 3600) / 60;
-        let seconds = (self.length % 3600) % 60;
-
-        return format!("{}:{}:{}", hours, minutes, seconds);
-    }
-
-    pub fn get_length(&self) -> u32 { self.length }
 }

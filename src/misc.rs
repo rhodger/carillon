1d37a3e8fdf105e1c870b4a3b0045eb948fb0a29
Suede <ymp17kwu@uea.ac.uk>
2020-01-29 19:07:43 +0000

    Added Length contents and methods
    + Length functionality
    + Test main

--- src/misc.rs
+++ src/misc.rs
@@ -1,7 +1,23 @@
-struct Length {
-    length: i32
+pub struct Length {
+    length: u32
 }
 
 impl Length {
-    fn get_length(&self) -> i32 { self.length }
+    pub fn from_int(x: u32) -> Length {
+        let length: Length = Length {
+            length: x
+        };
+
+        return length;
+    }
+
+    pub fn pretty_print(&self) -> String {
+        let hours = self.length / 3600;
+        let minutes = (self.length % 3600) / 60;
+        let seconds = (self.length % 3600) % 60;
+
+        return format!("{}:{}:{}", hours, minutes, seconds);
+    }
+
+    pub fn get_length(&self) -> u32 { self.length }
 }

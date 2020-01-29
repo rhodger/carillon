2520efff07df32f156b2108dc328adceb9fcd44a
Suede <ymp17kwu@uea.ac.uk>
2020-01-29 23:54:52 +0000

    Refactored
    ~ Moved tests to end of file

--- src/song.rs
+++ src/song.rs
@@ -1,5 +1,38 @@
 use super::misc::Length;
 
+pub struct Song {
+    title: String,
+    length: Length
+}
+
+impl Song {
+    pub fn new(t: &str, l: u32) -> Song {
+        let song = Song {
+            title: t.to_string(),
+            length: Length::from_int(l)
+        };
+
+        return song;
+    }
+
+    pub fn from_title(title: &str) -> Song{
+        Song {
+            title: title.to_string(),
+            length: Length::from_int(0)
+        }
+    }
+
+    pub fn pretty_print(&self) -> String {
+        format!("{} : {}", self.title, self.length.pretty_print())
+    }
+
+
+    //Getters
+    pub fn get_title(&self) -> String { self.title.to_string() }
+
+    pub fn get_length(&self) -> Length { self.length.clone() }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -33,37 +66,4 @@ mod tests {
         assert_eq!(song.get_title(), "All Star".to_string());
         assert_eq!(song.get_length().get_length(), 120);
     }
-}
-
-pub struct Song {
-    title: String,
-    length: Length
-}
-
-impl Song {
-    pub fn new(t: &str, l: u32) -> Song {
-        let song = Song {
-            title: t.to_string(),
-            length: Length::from_int(l)
-        };
-
-        return song;
-    }
-
-    pub fn from_title(title: &str) -> Song{
-        Song {
-            title: title.to_string(),
-            length: Length::from_int(0)
-        }
-    }
-
-    pub fn pretty_print(&self) -> String {
-        format!("{} : {}", self.title, self.length.pretty_print())
-    }
-
-
-    //Getters
-    pub fn get_title(&self) -> String { self.title.to_string() }
-
-    pub fn get_length(&self) -> Length { self.length.clone() }
 }
